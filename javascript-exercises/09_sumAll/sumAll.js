7d74fcc2724654ce7898cdd27dbdc408a8143c23
sawertynn <sawertynn@gmail.com>
2025-10-10 10:12:46 +0200

    09 sumAll done

--- javascript-exercises/09_sumAll/sumAll.js
+++ javascript-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // x = parseInt(a)
+    // y = parseInt(b)
+    // if (x !== a || y !== b) {
+    //     return 'ERROR'
+    // }
+    if (!Number.isInteger(a) || a < 0 || !Number.isInteger(b) || b< 0) {
+        return 'ERROR';
+    }
+    low = Math.min(a, b)
+    high = Math.max(a, b)
 
+    return (a + b) / 2 * (high - low + 1);
 };
 
 // Do not edit below this line

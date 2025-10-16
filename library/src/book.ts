87f58aed90ce3aac1c1f79534e5a5826e1b30cb1
sawertynn <sawertynn@gmail.com>
2025-10-16 11:12:56 +0200

    library project init commit

---
+++ library/src/book.ts
@@ -0,0 +1,19 @@
+export class Book {
+    title: string;
+    author: string;
+    pageNumber: number;
+    isRead: boolean;
+
+    constructor (title: string, author: string, pageNumber: number) {
+        this.title = title;
+        this.author = author;
+        this.pageNumber = pageNumber;
+        this.isRead = false;
+    }
+
+    info(): string {
+        const description = `${this.title} by {${this.author}, ${this.pageNumber} pages}`
+        const status = this.isRead ? 'already read' : 'not read yet';
+        return `${description}, ${status}`
+    }
+}
---
+++ library/src/main.ts
@@ -0,0 +1,5 @@
+import { Book } from './book.js'
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295);
+
+console.log(theHobbit.info());
